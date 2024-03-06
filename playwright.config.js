f9a36e2aa6cd3dd4494095cbe93f2c71457e5e09
Girish Nair <nairgirish100@gmail.com>
2024-03-06 22:21:41 +0400

    updated reporter

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,8 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', {  outputFile: 'test-results.json' }]
+    ['json', {  outputFile: 'test-results.json' }],
+    ['junit', { outputFile: 'results.xml' }]
   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
